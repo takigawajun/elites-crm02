0afe02951bf058ed1a7271e3bf25d8bc7f98618e
Jun Takigawa <takigawa-jun@av.avex.co.jp>
2016-11-03 13:43:40 +0000

    customerno

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,22 +1,54 @@
 class CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def new
+    @customer = Customer.new
   end
 
-  def create
-  end
+#------------------------下記を追加--------------------^
+ def create
+    @customer = Customer.new(customer_params)
+#-----------------------下記の内容に修正-------------------------
+    if  @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
+#-------------------------------------------------------
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if  @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to root_path
+  end
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+      )
   end
 end
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+100.times do |index|
+  Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
+end# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
