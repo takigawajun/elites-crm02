2aa168ff44a68f9e5b49f3c50a4be1e7e29cd07f
Jun Takigawa <takigawa-jun@av.avex.co.jp>
2016-11-03 13:50:43 +0000

    ダミーデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
 100.times do |index|
-  Customer.create(family_name: "鈴木", given_name: "太郎", email: "customer_#{index}@sparta.com")
-end# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+  Faker::Config.locale = :ja
+  family_name = Faker::Name.last_name
+  given_name = Faker::Name.first_name
+
+  Faker::Config.locale = :en
+  email = Faker::Internet.email
+
+  Customer.create(
+    family_name: family_name,
+    given_name: given_name,
+    email: email
+  )
+end
